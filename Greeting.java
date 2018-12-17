d6d570bfb1b5aed687208d6b9bcc749fc7ae8996
JDNET\sm21431 <minhajsyed@johndeere.com>
2018-12-16 20:26:50 -0600

    First Commit

---
+++ Greeting.java
@@ -0,0 +1,15 @@
+public class Greeting{
+
+	public static void main(String[] args) {
+		
+		int n = Integer.parseInt(args[0]);
+		int sum = 0;
+		for(int i=1; i<=n; i++){
+			sum += i;
+		}
+			
+		System.out.println("The sum of first "+n+" numbers is "+sum);
+		
+	}
+
+}
